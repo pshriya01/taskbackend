26a770e3ab66420bc328eff6f0736d605206ccf4
pshriya01 <shriya.shriya16@gmail.com>
2024-01-15 11:07:47 +0530

    trying again

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express")
 require("dotenv").config()
-const app = express()
 const Port = process.env.PORT || 8000
 const connection = require('./Config/db')
 const cors = require("cors")
 const { userRouter } = require("./Routers/UserRouter")
 const { taskRoutes } = require("./Routers/TaskRouter")
-
+const app = express()
 app.use(express.json())
 app.use(cors())
 app.use("/users",userRouter)
